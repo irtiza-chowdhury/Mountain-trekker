4d759840c4f8458ab4218e6b88d945a3a7a4be56
irtiza-chowdhury <irtizachowdhury5@gmail.com>
2022-11-21 23:10:33 +0600

    Initial commit

---
+++ asset/jquery_js/custom.js
@@ -0,0 +1,39 @@
+$('#Car').owlCarousel({
+    loop:true,
+    stagePadding: 0,
+    margin:10,
+    dots:false,
+    nav:true,
+    slideBy:2,
+    responsive:{
+        0:{
+            items:1
+        },
+        738:{
+            items:2
+        },
+        1100:{
+            items:3
+        }
+    }
+})
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     stagePadding: 0,
+//     margin:10,
+//     dots:false,
+//     nav:true,
+//     slideBy:2,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         738:{
+//             items:2
+//         },
+//         1100:{
+//             items:3
+//         }
+//     }
+// })
+
---
+++ asset/jquery_js/newCarousal.js
@@ -0,0 +1,20 @@
+// var olll=document.getElementById('olll');
+
+$('#TeamCarousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    nav:true,
+    slideBy:2,
+    responsive:{
+        0:{
+            items:1
+        },
+        738:{
+            items:2
+        },
+        1100:{
+            items:3
+        }
+    }
+})
